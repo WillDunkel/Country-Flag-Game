3f02bbeb79a9eae3ade507c363e84776b149da28
William Dunkel <wndunkel@gmail.com>
2024-01-10 09:29:17 -0600

    Initial Commit

---
+++ Country Flag Game/Country_Flag_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Country_Flag_GameApp.swift
+//  Country Flag Game
+//
+//  Created by William Dunkel on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Country_Flag_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Country Flag GameUITests/Country_Flag_GameUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Country_Flag_GameUITestsLaunchTests.swift
+//  Country Flag GameUITests
+//
+//  Created by William Dunkel on 1/10/24.
+//
+
+import XCTest
+
+final class Country_Flag_GameUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
